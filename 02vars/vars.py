82f74e2e3bb0926312b0cb3f152c34d8af62acd4
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-10-17 12:16:05 +0000

    added 02 vars file

---
+++ 02vars/vars.py
@@ -0,0 +1,46 @@
+# vars definition
+name = "Juan Jesus"
+age = 31
+country = "Spain"
+
+# Show information
+print("name: ", name)
+print("age: ", age)
+print("country: ", country)
+
+# update vars
+name = "Juan Jesus v2"
+age = 100
+
+# Show information
+print("name: ", name)
+print("age: ", age)
+print("country: ", country)
+
+# dynamic type
+age = "30v2"
+print("age: ", age)
+
+
+# types
+
+## integer
+age = 31
+negative = -20
+
+## float
+size = 1.75
+PI = 3.1416 # constant in python
+
+## str
+name = "Juan Jesus"
+name2 = 'Juan Jesus'
+name3 = """Juan
+Jesus
+Martin
+Melero"""
+
+print(name3)
+## bool
+isUser = True
+isWoman = False
