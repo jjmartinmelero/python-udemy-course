6d49e046a16adfc36ad03c665b5bd38a8b35ab00
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-10-20 09:26:48 +0000

    added exercises in section 02

--- 02vars/exercises.py
+++ 02vars/exercises.py
@@ -1,10 +1,10 @@
-# EX 1
+# ************************************** EX 1
 name = input("Your name:")
 age = int(input("age: "))
 country = input("Country: ")
 print(f'Hi {name}, {age} from {country}')
 
-# EX 2
+# ************************************** EX 2
 # generate uid 
 import random
 # from random import randint
@@ -15,6 +15,12 @@ year_of_birth = input("Year of birth YYYY: ")
 
 # build base uid
 uid_base = f"{name[:2].upper()}{last_name[:2].upper()}{year_of_birth[2:]}"
+
 # append a random 4-digit number
 random_suffix = random.randint(1000, 9999)
 print(f"{uid_base}{random_suffix}")
+
+# ************************************** generator email
+name = input("Your name:")
+last_name = input("Last name: ")
+print(f"{name.replace(" ", "").lower()}.{last_name.replace(" ", "").lower()}@work.com")
