9854e89113e1a62077a242365c625383cfbbb592
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-10-21 07:31:56 +0000

    removed comments

--- 06collections/exercises.py
+++ 06collections/exercises.py
@@ -1,67 +1,67 @@
 # EX 1 ---------------------------------------------------------
-# print("Playlist app")
+print("Playlist app")
 
-# albums = []
+albums = []
 
-# n_songs = int(input("Number of songs to add: "))
+n_songs = int(input("Number of songs to add: "))
 
-# for _ in range(n_songs):
-#     albums.append(input("Song: "))
+for _ in range(n_songs):
+    albums.append(input("Song: "))
 
-# # order
-# albums.sort(reverse=True)
+# order
+albums.sort(reverse=True)
 
-# # show list
-# print("Albums: ")
+# show list
+print("Albums: ")
 
-# for song in albums:
-#     print(song)
+for song in albums:
+    print(song)
 
 
 # EX 2 ---------------------------------
-# print("Clasifications app")
+print("Clasifications app")
 
-# califications = []
+califications = []
 
-# n_califications = int(input("Number of califications: "))
+n_califications = int(input("Number of califications: "))
 
-# for _ in range(n_califications):
-#     califications.append(int(input("Calification: ")))
+for _ in range(n_califications):
+    califications.append(int(input("Calification: ")))
 
-# average = 0
+average = 0
 
-# # sum(list)
+# sum(list)
 
-# for calification in califications:
-#     average += calification
+for calification in califications:
+    average += calification
 
-# print(f"average: {average / n_califications}")
+print(f"average: {average / n_califications}")
 
 
 # EX 3 -------------------------------------------
-# print("Suscriptions app")
-# subscribers = set()
+print("Suscriptions app")
+subscribers = set()
 
-# n_suscribers = int(input("n suscribers: "))
+n_suscribers = int(input("n suscribers: "))
 
-# for _ in range(n_suscribers):
-#     subscribers.add(input("email suscriber: "))
+for _ in range(n_suscribers):
+    subscribers.add(input("email suscriber: "))
 
-# print(subscribers)
+print(subscribers)
 
-# new_suscriber = input("new suscriber: ")
+new_suscriber = input("new suscriber: ")
 
-# if new_suscriber in subscribers:
-#     print("you can't add that suscriber :(")
-# else:
-#     subscribers.add(new_suscriber)
-#     print("addded !")
+if new_suscriber in subscribers:
+    print("you can't add that suscriber :(")
+else:
+    subscribers.add(new_suscriber)
+    print("addded !")
 
-# print(subscribers)
+print(subscribers)
 
-# remove_subscriber = input("Remove suscriber: ")
-# subscribers.remove(remove_subscriber)
+remove_subscriber = input("Remove suscriber: ")
+subscribers.remove(remove_subscriber)
 
-# print(subscribers)
+print(subscribers)
 
 
