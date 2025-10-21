f9a5c2d72e2b1d3835a302743f90e3e0eadb17fc
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-10-21 09:23:09 +0000

    removed comments

--- 07functions/exercises.py
+++ 07functions/exercises.py
@@ -1,37 +1,37 @@
 # EX 1 --------------------------------
 
-# print("get coordinates")
+print("get coordinates")
 
-# def get_coordinates():
-#     x, y, z = 10, 20, 30
+def get_coordinates():
+    x, y, z = 10, 20, 30
 
-#     # return (x,y,z)
-#     return x,y,z
+    # return (x,y,z)
+    return x,y,z
 
-# result = get_coordinates()
+result = get_coordinates()
 
-# print(result)
+print(result)
 
-# x,y,z = get_coordinates()
+x,y,z = get_coordinates()
 
-# print(x,y,z)
+print(x,y,z)
 
 # EX 2 sum with several args
-# def custom_sum(*args):
-#     total = 0
-#     for num in args:
-#         total += num
+def custom_sum(*args):
+    total = 0
+    for num in args:
+        total += num
     
-#     return total
+    return total
 
-# print(custom_sum(2,3,4,5,6))
+print(custom_sum(2,3,4,5,6))
 
 # EX 3 kwargs to print info
-# def custom_fn(**kwargs):
-#     for key, value in kwargs.items():
-#         print(f"key: {key} - value: {value}")
+def custom_fn(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key} - value: {value}")
 
-# custom_fn(name="jjmartinmelero", age=31)
+custom_fn(name="jjmartinmelero", age=31)
 
 def is_even_number(number):
     return number % 2 == 0
