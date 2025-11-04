d2244f9c7dc8a7621cfa578002d465a794e133db
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-11-04 11:57:24 +0000

    added edit endpoint

--- 13web/app.py
+++ 13web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from client_dao import ClientDAO
 from client import Client
 from client_form import ClientForm
@@ -20,6 +20,33 @@ def home():
     # client form empty
     client = Client()
     client_form = ClientForm(obj=client)
+    return render_template('index.html', title=title_app, clients=clients_db, client_form=client_form)
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    print('SAVE METHOD')
+    client = Client()
+    client_form = ClientForm(obj=client)
+    # print(client_form.errors)
+
+    if client_form.validate_on_submit():
+        client_form.populate_obj(client)
+        ClientDAO.insert(client)
+
+    return redirect(url_for('home'))
+
+@app.route('/clear')
+def clear():
+    return redirect(url_for('home'))
+
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    client = ClientDAO.select_by_id(id)
+    client_form = ClientForm(obj=client)
+    clients_db = ClientDAO.select()
+
     return render_template('index.html', title=title_app, clients=clients_db, client_form=client_form)
 
 
--- 13web/client_dao.py
+++ 13web/client_dao.py
@@ -5,6 +5,7 @@ from client import Client
 class ClientDAO:
 
     SELECT = 'SELECT * FROM customers ORDER BY id'
+    SELECT_ID = 'SELECT * FROM customers WHERE id=%s'
     INSERT = 'INSERT INTO customers(name, last_name, membership) VALUES (%s, %s, %s)'
     UPDATE = 'UPDATE customers SET name=%s, last_name=%s, membership=%s WHERE id=%s'
     DELETE = 'DELETE FROM customers WHERE id=%s'
@@ -24,6 +25,26 @@ class ClientDAO:
                 clients.append(client)
             
             return clients
+        except Exception as e:
+            print(f'An error ocurred: {e}') 
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.clear_conexion(conexion)
+
+    @classmethod
+    def select_by_id(cls, id):
+        conexion = None
+        try:
+            conexion = Conexion.get_conexion()
+            cursor = conexion.cursor()
+            values = (id,)
+            cursor.execute(cls.SELECT_ID, values)
+            result = cursor.fetchone()
+            
+            client = Client(result[0], result[1], result[2], result[3])
+            
+            return client
         except Exception as e:
             print(f'An error ocurred: {e}') 
         finally:
