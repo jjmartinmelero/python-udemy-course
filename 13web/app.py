239c056c8b654f84212cf6b4926728e578e28ec0
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-11-04 10:20:41 +0000

    added flask-wtforms

--- 13web/app.py
+++ 13web/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 from client_dao import ClientDAO
+from client import Client
+from client_form import ClientForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secretkey'
 
 
 title_app = 'GYM APP Python'
@@ -12,7 +16,11 @@ def home():
     app.logger.debug('home path')
     # get clients from db
     clients_db = ClientDAO.select()
-    return render_template('index.html', title=title_app, clients=clients_db)
+    
+    # client form empty
+    client = Client()
+    client_form = ClientForm(obj=client)
+    return render_template('index.html', title=title_app, clients=clients_db, client_form=client_form)
 
 
 if __name__ == '__main__':
